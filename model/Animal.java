4000b6612aa76c03dcc9eb640255b79bb78ff44e
ZordonLord <zordonlord@mail.ru>
2025-04-13 20:36:25 +1000

    Добавлен класс Animal

---
+++ model/Animal.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Animal {
+    private String name;
+    private LocalDate birthDate;
+    private int age;
+    private String gender;
+
+    public Animal(String name, LocalDate birthDate, int age, String gender) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+} 
