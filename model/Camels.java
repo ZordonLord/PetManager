7f2e5c00ca99431df4374b6250586039d34550af
ZordonLord <zordonlord@mail.ru>
2025-04-13 21:19:10 +1000

    Добавлен класс Camels

---
+++ model/Camels.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Camels extends PackAnimals {
+    private int humps; // количество горбов
+
+    public Camels(String name, LocalDate birthDate, int age, String gender, double speed, double carryingCapacity,
+                 int humps) {
+        super(name, birthDate, age, gender, speed, carryingCapacity);
+        this.humps = humps;
+    }
+
+    public int getHumps() {
+        return humps;
+    }
+
+    public void setHumps(int humps) {
+        this.humps = humps;
+    }
+
+    @Override
+    public String toString() {
+        return "Camels{" +
+                "name='" + getName() + '\'' +
+                ", birthDate='" + getBirthDate() + '\'' +
+                ", age=" + getAge() +
+                ", gender='" + getGender() + '\'' +
+                ", speed=" + getSpeed() +
+                ", carryingCapacity=" + getCarryingCapacity() +
+                ", humps=" + humps +
+                '}';
+    }
+} 
